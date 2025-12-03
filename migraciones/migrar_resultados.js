cb035593c30724843213429500cd230665f95953
marcos <marcos>
2025-12-03 14:39:17 +0100

    fgns

---
+++ migraciones/migrar_resultados.js
@@ -0,0 +1,19 @@
+const db = require("../db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS resultados (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      chatId TEXT,
+      tema TEXT,
+      aciertos INTEGER,
+      fallos INTEGER,
+      fecha TEXT
+    )
+  `, (err) => {
+    if (err) console.error("ERROR creando tabla resultados:", err);
+    else console.log("Tabla 'resultados' creada o ya existe.");
+  });
+});
+
+db.close();
